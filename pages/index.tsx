2a6a75d27703660be8c926036b9eb1b61022d60c
jgen16go <asai.work.usr@gmail.com>
2019-02-11 03:02:54 +0900

    update

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import { App } from '../container/App'
+// export default () => (
+//   <div>
+//     <App />
+//   </div>
+// )
+
+import * as React from 'react'
+
+export default () => <div>Index</div>
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { App } from '../container/App'
-export default () => (
-  <div>
-    <App />
-  </div>
-)
